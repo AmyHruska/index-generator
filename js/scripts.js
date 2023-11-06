d7bb660a04930af89bc7f8086120ce1186640b37
AmyHruska <amyhruskacda@gmail.com>
2023-11-06 10:13:10 -0800

    add basic file structures

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+window.onload =  function() {
+    let generate = document.getElementById("generate");
+    generate.addEventListener("submit", function(event){
+        event.preventDefault();
+        const 
+    })
+}
